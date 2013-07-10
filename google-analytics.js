d0666ff7afddd02c5ae44e66e44d42d8d67affd4
Raphael Pierquin <raphael@ut7.fr>
2013-07-10 12:21:18 +0200

    corrige google analytics

--- google-analytics.js
+++ google-analytics.js
@@ -3,13 +3,12 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-42365063-1', 'leanagilecamp.fr'
-);
+ga('create', 'UA-42365063-1', 'leanagilecamp.fr');
 ga('send', 'pageview');
 
 function trackOutboundLink(link, category, action) {
   try {
-    ga('event', category , action);
+    ga('send', 'event', category, action);
   } catch(err){}
 
   setTimeout(function() {
