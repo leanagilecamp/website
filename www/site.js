63c2376b6aa4c7ccf97cf98593a65d336dfcdda9
Bruno Thomas <bruno@barreverte.fr>
2013-08-10 16:32:08 +0200

    ajout du mail de contact

---
+++ www/site.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+	$('#pour_nous_contacter').append(String.fromCharCode(99, 111, 110, 116, 97, 99, 116, 64, 108, 101, 97, 110, 97, 103, 105, 108, 101, 99, 97, 109, 112, 46, 102, 114));
+});
