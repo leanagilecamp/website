e1acdda0a16b66fbdd57c94452b52d1eb3ae93da
bamthomas <bruno@barreverte.fr>
2013-10-02 20:48:09 +0200

    migration bootstrap 2 vers 3 + menu affix avec retour à la ligne des intitulés de la table des matière -- BT + PB

--- www/nouveau_site/site.js
+++ www/nouveau_site/site.js
@@ -1,53 +1,59 @@
 $(document).ready(function () {
-	$('.pour_nous_contacter').append(String.fromCharCode(99, 111, 110, 116, 97, 99, 116, 64, 108, 101, 97, 110, 97, 103, 105, 108, 101, 99, 97, 109, 112, 46, 102, 114));
-	
-	$('#TOC').affix({
-            offset: 675
+    $('.pour_nous_contacter').append(String.fromCharCode(99, 111, 110, 116, 97, 99, 116, 64, 108, 101, 97, 110, 97, 103, 105, 108, 101, 99, 97, 109, 112, 46, 102, 114));
+
+    $('ul.nav li').click(function () {
+        var nav_id = $(this).attr('id');
+        var content_id = nav_id.replace('-navbar', '');
+        afficheOnglet(content_id);
+    });
+
+    $('.navbar-brand').on('click', function () {
+        afficheOnglet('accueil');
+    });
+
+    function afficheOnglet(id) {
+        $('.content').addClass('hidden');
+        $('#' + id).removeClass('hidden');
+        $('ul.nav li').removeClass('active');
+        $('#' + id + '-navbar').addClass('active');
+    }
+
+    $('ul.nav li:contains("Guide")').click(telechargeGuide);
+
+    function telechargeGuide() {
+        if ($("#conclusion").length == 0) {
+            $.get('guide.html', function (data) {
+                $('#leguide').append(data);
+                setActiveTocItem();
+            });
+        }
+    }
+
+    function setActiveTocItem() {
+        $('#toc-guide').find('li').on('click', function () {
+            $('#toc-guide').find('li').removeClass('active');
+            $(this).addClass('active');
         });
+    }
 
-	$('ul.nav li').click(function () {
-            var nav_id = $(this).attr('id');
-            var content_id = nav_id.replace('-navbar', '');
-	    afficheOnglet(content_id);
-        });
-	
-	$('.brand').on('click', function() {
-	    afficheOnglet('accueil');
-	});
+    $('.pdfButton').on('click', function () {
+        trackOutboundLink(this, 'inbound download', 'download pdf');
+        return false;
+    });
+    $('.epubButton').on('click', function () {
+        trackOutboundLink(this, 'inbound download', 'download epub');
+        return false;
+    });
+    $('.kindleButton').on('click', function () {
+        trackOutboundLink(this, 'inbound download', 'download azw3');
+        return false;
+    });
+    $('.htmlButton').on('click', function () {
+        trackOutboundLink(this, 'inbound html');
+        telechargeGuide();
+        afficheOnglet('leguide');
+        return false;
+    });
 
-	function afficheOnglet(id) {
-            $('.content').addClass('hidden');
-            $('#' + id).removeClass('hidden');
-            $('ul.nav li').removeClass('active');
-            $('#' + id + '-navbar').addClass('active');
-	}
 
-	$('ul.nav li:contains("Guide")').click(telechargeGuide);
-
-	function telechargeGuide() {
-	    if ($("#conclusion").length == 0) {
-	        $.get('guide.html', function(data) {
-	            $('#leguide').append(data);
-	        });
-	    }
-	}
-
-	$('.pdfButton').on('click', function () {
-	    trackOutboundLink(this, 'inbound download', 'download pdf'); 
-	    return false;
-	});
-	$('.epubButton').on('click', function () {
-	    trackOutboundLink(this, 'inbound download', 'download epub'); 
-	    return false;
-	});
-	$('.kindleButton').on('click', function () {
-	    trackOutboundLink(this, 'inbound download', 'download azw3'); 
-	    return false;
-	});
-	$('.htmlButton').on('click', function () {
-	    trackOutboundLink(this, 'inbound html'); 
-	    telechargeGuide();
-	    afficheOnglet('leguide'); 
-	    return false;
-	});
 });
