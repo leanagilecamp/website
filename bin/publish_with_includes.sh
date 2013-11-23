35e53914d74efffe12bd7978d32d63b7c83a6b39
Bruno Thomas <bruno@barreverte.fr>
2013-11-23 20:59:30 +0100

    suppression du chemin en dur bthomas pour www/index.html

--- bin/publish_with_includes.sh
+++ bin/publish_with_includes.sh
@@ -5,8 +5,8 @@ for include in output/www/*_include.html
 do
 	fichier_html=`echo $include |sed -e 's/_include//'`
 	echo $fichier_html
-	cat /home/bthomas/src/website/www/index.html | awk  -F '<!--#include page -->' 'BEGIN {RS="</html>\n"} {print $1}'  > $fichier_html
+	cat www/index.html | awk  -F '<!--#include page -->' 'BEGIN {RS="</html>\n"} {print $1}'  > $fichier_html
     cat $include >> $fichier_html
-	cat /home/bthomas/src/website/www/index.html | awk  -F '<!--#include page -->' 'BEGIN {RS="</html>\n"} {print $2"</html>"}'  >> $fichier_html
+	cat www/index.html | awk  -F '<!--#include page -->' 'BEGIN {RS="</html>\n"} {print $2"</html>"}'  >> $fichier_html
 done
 mv output/www/accueil.html output/www/index.html
