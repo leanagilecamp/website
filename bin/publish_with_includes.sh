9bd0d7dc58ce3e2fadb74a636710bef00efc0420
bamthomas <bruno@barreverte.fr>
2013-12-04 19:22:07 +0100

    Correction de l'include du site.js au lieu du fichier daté

--- bin/publish_with_includes.sh
+++ bin/publish_with_includes.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 mkdir -p output
 rsync -a www output/
+day=`date "+%Y%m%d"` && sed -i "s/site.js/site_$day.js/" output/www/index.html && mv output/www/site.js output/www/site_$day.js
 for include in output/www/*_include.html 
 do
 	fichier_html=`echo $include |sed -e 's/_include//'`
 	echo $fichier_html
-	cat www/index.html | awk  -F '<!--#include page -->' 'BEGIN {RS="</html>\n"} {print $1}'  > $fichier_html
+	cat output/www/index.html | awk  -F '<!--#include page -->' 'BEGIN {RS="</html>\n"} {print $1}'  > $fichier_html
     cat $include >> $fichier_html
-	cat www/index.html | awk  -F '<!--#include page -->' 'BEGIN {RS="</html>\n"} {print $2"</html>"}'  >> $fichier_html
+	cat output/www/index.html | awk  -F '<!--#include page -->' 'BEGIN {RS="</html>\n"} {print $2"</html>"}'  >> $fichier_html
 done
 mv output/www/accueil.html output/www/index.html
-day=`date "+%Y%m%d"` && sed -i "s/site.js/site_$day.js/" output/www/index.html && mv output/www/site.js output/site_$day.js
+
